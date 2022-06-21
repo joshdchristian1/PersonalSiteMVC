c588003bf8ce15336026b3d59a1edae4b5b82582
Josh Christian <joshdchristian1@outlook.com>
2022-06-20 21:36:40 -0500

    New Commit

---
+++ PersonalSite/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSite.Models;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace PersonalSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            return View();
+
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult ContactMe()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ PersonalSite/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PersonalSite.Models
+{
+    public class ContactViewModel
+    {
+        
+
+            //We can use Data Annotations to add validation to our model.  This is useful when we have required fields or need certain types of information.
+
+            [Required(ErrorMessage = "*Name is required")] // Makes the field required
+            public string Name { get; set; }
+            [Required(ErrorMessage = "*Email is required")]
+            [DataType(DataType.EmailAddress)] //Cetain formatting is expected (@ symbol, .com, etc.)
+            public string Email { get; set; }
+            [Required(ErrorMessage = "*Subject required")]
+            public string Subject { get; set; }
+            [Required(ErrorMessage = "*Message is required")]
+            [DataType(DataType.MultilineText)]//Makes the textbox for this field bigger
+            public string Message { get; set; }
+
+
+        
+    }
+}
